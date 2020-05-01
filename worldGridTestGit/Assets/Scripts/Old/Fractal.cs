5a9c284c7c5d4e25a605e69496c90945aff88b45
stijn koster <stijn.koster@mk18.nl>
2020-05-01 12:59:26 +0200

    Initial Commit
    Initial Commit with progres so far.
    includes a work in progres map editor

---
+++ worldGridTestGit/Assets/Scripts/Old/Fractal.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+    public int maxDepth;
+    public float childScale;
+
+    private int depth;
+    private Material[] materials;
+
+    private void InitializeMaterials()
+    {
+        materials = new Material[maxDepth + 1];
+        for (int i = 0; i <= maxDepth; i++)
+        {
+            float t = i / (maxDepth - 1f);
+            t *= t;
+            materials[i] = new Material(material);
+            materials[i].color = Color.Lerp(Color.white, Color.blue, t);
+        }
+        materials[maxDepth].color = Color.cyan;
+    }
+    private void Start()
+    {
+        if (materials == null)
+        {
+            InitializeMaterials();
+        }
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = materials[depth];
+
+        if (depth < maxDepth)
+        {
+            StartCoroutine(CreateChildren());
+        }
+    }
+
+    private static Vector3[] ChildDirections = {
+        Vector3.up,
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back
+    };
+
+    private static Quaternion[] ChildOrientations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f),
+        Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f),
+        Quaternion.Euler(-90f, 0f, 0f)
+    };
+
+    private void Initialize(Fractal parent, int childIndex)
+    {
+        mesh = parent.mesh;
+        materials = parent.materials;
+        maxDepth = parent.maxDepth;
+        childScale = parent.childScale;
+        depth = parent.depth + 1;
+
+        transform.parent = parent.transform;
+
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = ChildDirections[childIndex] * (0.5f + 0.5f * childScale);
+        transform.localRotation = ChildOrientations[childIndex];
+
+    }
+
+    private IEnumerator CreateChildren()
+    {
+        for (int i = 0; i < ChildDirections.Length; i++)
+        {
+            yield return new WaitForSeconds(Random.Range(0.3f, 0.8f));
+            new GameObject("Fractal Child").AddComponent<Fractal>().
+                Initialize(this, i);
+        }
+    }
+}
