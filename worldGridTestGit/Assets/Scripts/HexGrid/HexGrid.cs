50d29e4135a2753aa6f6b0b09a674814a9edb51f
stijn koster <stijn.koster@mk18.nl>
2020-06-19 11:58:13 +0200

    Added smooth curves and rotation to movement

--- worldGridTestGit/Assets/Scripts/HexGrid/HexGrid.cs
+++ worldGridTestGit/Assets/Scripts/HexGrid/HexGrid.cs
@@ -116,6 +116,20 @@ public class HexGrid : MonoBehaviour
         }
         return null;
     }
+    public List<HexCell> GetPath()
+    {
+        if (!currentPathExists) {
+            return null;
+        }
+        List<HexCell> path = ListPool<HexCell>.Get();
+        for (HexCell c = currentPathTo; c != currentPathFrom; c = c.PathFrom) {
+            path.Add(c);
+        }
+
+        path.Add(currentPathFrom);
+        path.Reverse();
+        return path;
+    }
     void CreateCells()
     {
         cells = new HexCell[cellCountZ * cellCountX];
@@ -248,7 +262,7 @@ public class HexGrid : MonoBehaviour
                 return true;
             }
 
-            int currentTurn = current.Distance / speed;
+            int currentTurn = (current.Distance - 1) / speed;
 
             for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
                 HexCell neighbor = current.GetNeighbor(d);
@@ -281,7 +295,7 @@ public class HexGrid : MonoBehaviour
                 }
 
                 int distance = current.Distance + moveCost;
-                int turn = distance / speed;
+                int turn = (distance - 1) / speed;
                 if (turn > currentTurn) {
                     distance = turn * speed + moveCost;
                 }
@@ -310,7 +324,7 @@ public class HexGrid : MonoBehaviour
         if (currentPathExists) {
             HexCell current = currentPathTo;
             while (current != currentPathFrom) {
-                int turn = current.Distance / speed;
+                int turn = (current.Distance - 1) / speed;
                 current.SetLabel(turn.ToString());
                 current.EnableHighlight(Color.white);
                 current = current.PathFrom;
